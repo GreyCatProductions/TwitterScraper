c558951800f707f47a4505ab3f21ee592df6eae4
GreyCatProductions <GreyCatProductionsOriginal@gmail.com>
2024-11-13 10:05:05 +0100

    V3

--- metricScrapeAdvancedWithLogin.py
+++ metricScrapeAdvancedWithLogin.py
@@ -2,110 +2,142 @@ import re
 import time
 
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-import os
 
-def get_metrics_login(url):
-    geckodriver_path = os.path.join(os.getcwd(), 'geckodriver.exe')
-    service = Service(geckodriver_path)
-    options = Options()
-    #options.add_argument("--headless")
-    driver = webdriver.Firefox(service=service, options=options)
+from commonMethods import extract_post_id, twitter_time_to_python_time, Tweet
 
-    def login():
+
+def login(driver):
+    try:
+        driver.get("https://x.com/home")
+        time.sleep(5)  # manually close firefox windows
+        # Wait for the page to load and locate the login button
+        login_button = WebDriverWait(driver, 300).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='Anmelden']"))
+        )
+        time.sleep(1)
+        login_button.click()
+
+        # Locate and interact with email/username field
+        email_text = WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located(
+                (By.XPATH, "//span[text()='Telefonnummer, E-Mail-Adresse oder Nutzername']"))
+        )
+        email_field = email_text.find_element(By.XPATH, "ancestor::*[4]")
+        time.sleep(1)
+        email_field.click()
+        driver.switch_to.active_element.send_keys("shapovalov@connected-organization.de")
+
+        # Click "Weiter" to proceed
+        next_button = WebDriverWait(driver, 300).until(
+            EC.element_to_be_clickable((By.XPATH, "//span[text()='Weiter']/ancestor::*[2]"))
+        )
+        next_button.click()
+
+        # Handle unusual activity prompt if it appears
         try:
-            # Wait for the page to load and locate the login button
-            login_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "//span[text()='Anmelden']"))
-            )
-            time.sleep(1)
-            login_button.click()
-
-            # Locate and interact with email/username field
-            email_text = WebDriverWait(driver, 10).until(
+            unusual_activity = WebDriverWait(driver, 300).until(
                 EC.presence_of_element_located(
-                    (By.XPATH, "//span[text()='Telefonnummer, E-Mail-Adresse oder Nutzername']"))
+                    (By.XPATH, "//span[contains(text(), 'ungewöhnliche Anmeldeaktivität')]"))
             )
-            email_field = email_text.find_element(By.XPATH, "ancestor::*[4]")
-            time.sleep(1)
-            email_field.click()
-            driver.switch_to.active_element.send_keys("shapovalov@connected-organization.de")
-
-            # Click "Weiter" to proceed
-            next_button = WebDriverWait(driver, 10).until(
+            driver.switch_to.active_element.send_keys("OlegShap05")
+            next_button = WebDriverWait(driver, 300).until(
                 EC.element_to_be_clickable((By.XPATH, "//span[text()='Weiter']/ancestor::*[2]"))
             )
             next_button.click()
+        except:
+            print("No unusual activity prompt detected")
 
-            # Handle unusual activity prompt if it appears
-            try:
-                unusual_activity = WebDriverWait(driver, 5).until(
-                    EC.presence_of_element_located(
-                        (By.XPATH, "//span[contains(text(), 'ungewöhnliche Anmeldeaktivität')]"))
-                )
-                driver.switch_to.active_element.send_keys("OlegShap05")
-                next_button = WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//span[text()='Weiter']/ancestor::*[2]"))
-                )
-                next_button.click()
-            except:
-                print("No unusual activity prompt detected")
-
-            # Enter password and complete login
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//span[text()='Passwort']"))
-            )
-            driver.switch_to.active_element.send_keys("Twitter48311!")
-            final_login_button = WebDriverWait(driver, 10).until(
-                EC.element_to_be_clickable((By.XPATH, "(//span[text()='Anmelden'])[2]/ancestor::*[3]"))
-            )
-            final_login_button.click()
-            return True
-        except Exception as e:
-            print("Login failed:", e)
-            return False
-    try:
-        driver.get(url)
-
-        if not login():
-            return False
-
-        WebDriverWait(driver, 10).until( #some path that checks if side is loaded
-            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/section/div/div/div[1]/div/div/article/div/div/div[3]/div[5]/div/div/div[1]/button/div/div[2]/span/span/span"))
+        # Enter password and complete login
+        WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.XPATH, "//span[text()='Passwort']"))
         )
+        driver.switch_to.active_element.send_keys("Twitter48311!")
 
-        html_content = driver.page_source
-        soup = BeautifulSoup(html_content, 'html.parser')
+        time.sleep(1)
+        final_login_button = WebDriverWait(driver, 300).until(
+            EC.element_to_be_clickable((By.XPATH, "(//span[text()='Anmelden'])/ancestor::*[3]"))
+        )
+        final_login_button.click()
+        time.sleep(3)
+        return True
+    except Exception as e:
+        print("Login failed:", e)
+        return False
 
-        counts = {
-            "replies": 0,
-            "reposts": 0,
-            "likes": 0,
-            "bookmarks": 0,
-            "views": 0
-        }
+def get_metrics_login(url, driver, get_replies): #login before using this function needed
+    driver.get(url)
 
-        data = soup.find(attrs={"aria-label": re.compile(r"replies|reposts|likes|bookmarks|views")})
+    #scroll_to_bottom(driver)
 
-        if data:
-            data_text = data.get("aria-label")
-            pattern = r"(\d+)\s*(replies|reposts|likes|bookmarks|views)"
-            matches = re.findall(pattern, data_text)
+    time.sleep(5)
 
-            for count, label in matches:
-                counts[label] = int(count)
+    html_content = driver.page_source
+    soup = BeautifulSoup(html_content, 'html.parser')
 
-        reply_count = counts["replies"]
-        repost_count = counts["reposts"]
-        like_count = counts["likes"]
-        bookmark_count = counts["bookmarks"]
-        view_count = counts["views"]
+    data = soup.findAll(attrs={"aria-label": re.compile(r"replies|reposts|likes|bookmarks|views")})
+    time_stamps = soup.find_all('time', datetime=True) #time only works as long as their only occurences are on the posts
 
-        return reply_count, repost_count, like_count, bookmark_count, view_count
-    finally:
-        driver.quit()
+    if len(data) != len(time_stamps):
+        print("data and time not synchrone!!!")
+
+    if data is None:
+        print("No data element found")
+        return 0, 0, 0, 0, 0
+
+    post_data = data[0]
+    reply_count, repost_count, like_count, bookmark_count, view_count = get_metrics(post_data)
+    time_of_post = time_stamps[0].text
+    post_id = extract_post_id(url)
+
+    og_tweet = Tweet(reply_count, repost_count, like_count, bookmark_count, view_count, "",
+                         time.time() - twitter_time_to_python_time(time_of_post) / 60)
+
+    replies = []
+    #region replies
+    if get_replies:
+        replies_data = data[1:]
+        time_stamps_data = time_stamps[1:]
+        for reply_data in replies_data:
+            time_text = "0"
+            reply_count, repost_count, like_count, bookmark_count, view_count = get_metrics(reply_data)
+            replies.append(Tweet(reply_count, repost_count, like_count, bookmark_count, view_count, post_id, "0"))
+    #endregion
+    print("replies found " + str(len(replies)))
+    return og_tweet, replies
+
+def get_metrics(data):
+    data_text = data.get("aria-label")
+    replies_match = re.search(r"(\d+)\s+replies", data_text)
+    reposts_match = re.search(r"(\d+)\s+reposts", data_text)
+    likes_match = re.search(r"(\d+)\s+likes", data_text)
+    bookmarks_match = re.search(r"(\d+)\s+bookmarks", data_text)
+    views_match = re.search(r"(\d+)\s+views", data_text)
+
+    reply_count = repost_count = like_count = bookmark_count = view_count = 0
+
+    if replies_match:
+        reply_count = int(replies_match.group(1))
+    if reposts_match:
+        repost_count = int(reposts_match.group(1))
+    if likes_match:
+        like_count = int(likes_match.group(1))
+    if bookmarks_match:
+        bookmark_count = int(bookmarks_match.group(1))
+    if views_match:
+        view_count = int(views_match.group(1))
+    return reply_count, repost_count, like_count, bookmark_count, view_count
+
+
+def scroll_to_bottom(driver, scroll_pause_time=2):
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(scroll_pause_time)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
--- twitterScrapeMain.py
+++ twitterScrapeMain.py
@@ -1,5 +1,4 @@
 import logging
-import time
 
 from commonMethods import *
 from metricScrapeNoLogin import *
@@ -14,37 +13,51 @@ logging.basicConfig(
 )
 #endregion
 
-def scrape_with_reply_count(url, minutes_passed):
+def scrape_with_reply_count(urls, driver, start_date, all_layers):
+    for url in urls:
+        try:
+            logging.info("Scraping metrics for URL: %s", url)
+            og_tweet, replies = get_metrics_login(url, driver, all_layers)
+
+            dir_path = os.path.join("data", start_date, str(extract_post_id(url)))
+            os.makedirs(dir_path, exist_ok=True)
+            file_path_og_post = os.path.join(dir_path, "og_post.csv")
+            file_path_replies = os.path.join(dir_path, "replies.csv")
+            save_to_csv_login(file_path_og_post, file_path_replies, og_tweet, replies)
+
+            logging.info("Done successfully")
+
+        except Exception as e:
+            logging.error("Error scraping metrics for URL: %s", url, exc_info=True)
+
+def hourly_scrape(urls: [str], cycles: int, time_between_cycles: int, all_layers: bool):
     try:
-        logging.info("Scraping metrics for URL: %s", url)
-        reply_count, repost_count, like_count, bookmark_count, view_count = get_metrics_login(url)
-        spreading_rate = round((repost_count + reply_count) / view_count * 100, 5) if view_count > 0 else 0
+        #region preparing the driver and logging in
+        geckodriver_path = os.path.join(os.getcwd(), 'geckodriver.exe')
+        service = Service(geckodriver_path)
+        options = Options()
+        #options.add_argument("--headless")
+        driver = webdriver.Firefox(service=service, options=options)
+        if not login(driver):
+            logging.error("Login failed")
+        #endregion
 
-        logging.info(
-            "Metrics retrieved: Replies: %d, Reposts: %d, Likes: %d, Bookmarks: %d, Views: %d, Spreading rate: %.5f%%",
-            reply_count, repost_count, like_count, bookmark_count, view_count, spreading_rate
-        )
+        start_date = datetime.now().strftime('%Y-%m-%d')
+        minutes_passed = 0
 
-        post_id = extract_post_id(url)
-        metrics_data = {
-            "reply_count": reply_count,
-            "repost_count": repost_count,
-            "like_count": like_count,
-            "bookmark_count": bookmark_count,
-            "view_count": view_count,
-            "spreading_rate": spreading_rate
-        }
-        save_to_csv_login(post_id, metrics_data, minutes_passed)
+        for cycle in range(cycles):
+            logging.info("Cycle %d/%d starting", cycle + 1, cycles)
+            scrape_start = time.time()
+            scrape_with_reply_count(urls, driver, start_date, all_layers)
+            time_used_for_scraping = time.time() - scrape_start
 
-    except Exception as e:
-        logging.error("Error scraping metrics for URL: %s", url, exc_info=True)
+            minutes_passed += int(time_between_cycles / 60)
 
-def hourly_scrape_with_reply_count(url: str, cycles: int, time_between_cycles: int):
-    minutes_passed = 0
-    while cycles > 0:
-        time.sleep(time_between_cycles)
-        scrape_with_reply_count(url, minutes_passed)
-        minutes_passed += int(time_between_cycles / 60)
-        cycles -= 1
+            time.sleep(time_between_cycles - time_used_for_scraping)
+    finally:
+        driver.quit()
 
-hourly_scrape_with_reply_count("https://x.com/TIME/status/1854146717249130998", 72, 3600)
+
+hourly_scrape([
+    "https://x.com/TheFigen_/status/1854864984280334406", "https://x.com/gracieback2/status/1854786839028334886", "https://x.com/Travis_4_Trump/status/1854532605837787153"
+], 72, 600, False)
